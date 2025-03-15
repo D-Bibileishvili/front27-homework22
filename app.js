b94186c125f1641f9bb5e5c93a759bb4fbe9b0cd
D-Bibileishvili <d.bibileishvili19@gmail.com>
2025-03-15 16:09:27 +0400

    output: name, email and address

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
 fetch("https://jsonplaceholder.typicode.com/users")
-  .then((response) => response.json())
-  .then((data) => console.log(data))
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    return response.json();
+  })
+  .then((data) => {
+    const users = data.map((user) => ({
+      name: user.name,
+      email: user.email,
+      address: `${user.address.street}, ${user.address.city}, ${user.address.zipcode}`,
+    }));
+    console.log(users);
+  })
   .catch((error) => console.error("Error:", error));
