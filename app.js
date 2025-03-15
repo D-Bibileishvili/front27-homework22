b21623c45fd6acfb9c2edb1a6f4844ed167af6b0
D-Bibileishvili <d.bibileishvili19@gmail.com>
2025-03-15 17:07:16 +0400

    add class and change structure

--- app.js
+++ app.js
@@ -5,12 +5,21 @@ fetch("https://jsonplaceholder.typicode.com/users")
     }
     return response.json();
   })
+
   .then((data) => {
-    const users = data.map((user) => ({
-      name: user.name,
-      email: user.email,
-      address: `${user.address.street}, ${user.address.city}, ${user.address.zipcode}`,
-    }));
-    console.log(users);
+    const container = document.getElementById("user");
+    container.innerHTML = " ";
+
+    data.forEach((user) => {
+      const userCard = document.createElement("div");
+      userCard.classList.add("card");
+
+      userCard.innerHTML = `<h3>${user.name}</h3>
+        <p><strong>Email:</strong> ${user.email}</p>
+        <p><strong>Address:</strong> ${user.address.street}, ${user.address.city}, ${user.address.zipcode}</p>`;
+
+      container.appendChild(userCard);
+    });
   })
+
   .catch((error) => console.error("Error:", error));
